087203669edd03f09c94326e3c0cfa7cdccf466f
somesortofcheng <zefromcheng@gmail.com>
2026-08-23 15:16:52 -0600

    Update build-windows-clang.sh
    add -DHAS_MEMORY_BREAKPOINTS=ON \

--- .ci/build-windows-clang.sh
+++ .ci/build-windows-clang.sh
@@ -30,6 +30,7 @@ cmake ..                                               \
     -DCMAKE_INSTALL_PREFIX=/usr                        \
     -DUSE_NATIVE_INSTRUCTIONS=OFF                      \
     -DUSE_PRECOMPILED_HEADERS=OFF                      \
+    -DHAS_MEMORY_BREAKPOINTS=ON                        \
     -DCMAKE_C_FLAGS="$CFLAGS"                          \
     -DCMAKE_CXX_FLAGS="$CFLAGS"                        \
     -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAG}"          \
